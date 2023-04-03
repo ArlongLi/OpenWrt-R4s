ca78a7e6287d70e3a5b1bc04904a2cddd78552af
ArlongLi <48848529+ArlongLi@users.noreply.github.com>
2023-04-03 22:18:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,5 +16,5 @@
 sed -i 's/192.168.1.*.1/192.168.15.1/g' package/base-files/files/bin/config_generate
 
 
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon-mod/g' feeds/luci/collections/luci/Makefile
 
